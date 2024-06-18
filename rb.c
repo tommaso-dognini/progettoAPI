96e229b34d68a85a1839c6f544d1ca7ced780519
tommaso <tommaso.dognini@gmail.com>
2024-06-18 17:51:54 +0200

    alberi funzionanti -manca free delle cancellazioni e alcuni fix nel fixuprecolor

--- rb.c
+++ rb.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// dichiaro la struttura nodo
+// --- ALBERI ROSSO NERI ---
+
+// Definisco la struttura di un Nodo
 typedef struct Nodo
 {
     char colore; // 'r' = rosso , 'n' = nero
@@ -10,23 +12,64 @@ typedef struct Nodo
     struct Nodo *sx;
     struct Nodo *padre;
 } Nodo;
-// inizializzo la setinella nill (il valore della chiave e irrilevante)
-Nodo NILL = {'n', 0, &NILL, &NILL, &NILL};
 
+// Struct per arrichire albero...
+typedef struct Dati
+{
+    int valore;
+} Dati;
+
+// Definisco il nodo sentilenna NILL
+//  Per semplificazione di condizioni al contorno invece che usare tipo NULL
+// impongo che tutte le foglie puntano a un nodo NILL sentinella
+// chiave e colore sono indifferenti... Mi interessa che punti a se stesso (NILL)
+Nodo NILL = {'n', -999, &NILL, &NILL, &NILL};
+
+// FUNZIONI DI GESTIONE ALBERI ROSSO-NERI
+
+// Funzione per la creazione di un nodo -> Malloc della struct con chiave data e puntatori a NILL
 Nodo *crea_nodo(int chiave);
+
+// inserimento come se fosse bst di nodo nell'albero -> restituisce l'abero = puntatore alla radice
 void inserisci(Nodo **albero, int chiave);
+
+// Funzione per aggiustare albero e mantenere bilanciamento e proprieta R-B dopo inserimento di un nodo
 void inserisci_fixup(Nodo **albero, Nodo *nuovo_nodo);
+
+// Funzioni per la rotazione sx e dx attorno al nodo perno
 void ruota_dx(Nodo **albero, Nodo *nodo_perno);
 void ruota_sx(Nodo **albero, Nodo *nodo_perno);
+
+// Funzione ausiliaria per stampare l'albero in senso crescente
 void stampa_in_ordine(Nodo *albero);
 
+// Funzione per impiantare un sotto albero ad un nodo
+void trapianta(Nodo **albero, Nodo *u, Nodo *v);
+
+// Funzione che restituisce il minimo (puntatore al nodo) del sotto albero del nodo passato.
+Nodo *minimo(Nodo *nodo);
+
+// Funzione che restituisce il massimo (puntatore al nodo) del sotto albero del nodo passato
+Nodo *massimo(Nodo *nodo);
+
+// Funzione che restituisce il puntatore del primo nodo, se esiste, con la chiave inserita.
+Nodo *cerca(Nodo *radice, int chiave);
+
+// Funzione per eliminare un nodo (come fosse bst)
+void elimina_nodo(Nodo **albero, Nodo *nodo);
+
+// Funzione per aggiustare albero e mantere bilanciamento e proprieta r-d dopo eliminizione di un nodo
+void elimina_fixUp(Nodo **albero, Nodo *nodo);
+
+// MAIN PER TEST
 int main()
 {
     // Inizializzo alebero = puntatore alla radice
     Nodo *albero;
     albero = &NILL;
 
-    int chiavi[] = {1, 3, 4, 5 , 10 , 20, 33 , 18 , 7 , 9};
+    // test inserimento
+    int chiavi[] = {1, 3, 4, 5, 10, 20, 33, 18, 7, 9};
     // printf("Chiave: %d \n", &chiave);
     int i;
     for (i = 0; i < 10; i++)
@@ -35,6 +78,26 @@ int main()
     }
     printf("RADICE: %d \n", albero->chiave);
     stampa_in_ordine(albero);
+
+    // test minimo e massimo
+    printf("\n\n");
+    printf("Il minimo e: %d\n", minimo(albero)->chiave);
+    printf("Il massimo e: %d\n", massimo(albero)->chiave);
+
+    // test cerca
+    printf("\n\n");
+    Nodo *x;
+    int n = 7;
+    x = cerca(albero, n);
+    printf("Nodo cercato: %d\n", x->chiave);
+
+    // test cancella
+    printf("\n\n");
+    printf("ELimina nodo %d\n", n);
+    elimina_nodo(&albero, x);
+    printf("RADICE: %d \n", albero->chiave);
+    stampa_in_ordine(albero);
+
     return 0;
 }
 
@@ -79,48 +142,10 @@ void inserisci(Nodo **albero, int chiave)
     nuovo_nodo->sx = &NILL;
     nuovo_nodo->dx = &NILL;
     nuovo_nodo->colore = 'r';
+
     inserisci_fixup(albero, nuovo_nodo);
     return;
 }
-
-// void inserisci(Nodo **albero, int chiave)
-// {
-//     // creo il nodo
-//     Nodo *nuovo_nodo = crea_nodo(chiave);
-
-//     Nodo *x = *albero;
-//     Nodo *y = &NILL;
-
-//     // se albero vuoto imposto nuovo nodo come radice e lo coloro di nero
-//     if (x == &NILL)
-//     {
-//         (*albero) = nuovo_nodo;
-//         (*albero)->colore = 'n';
-//     }
-//     else
-//     {
-//         while (x != &NILL)
-//         {
-//             if (nuovo_nodo->chiave < x->chiave)
-//                 x = x->sx;
-//             else
-//                 x = x->dx;
-//         }
-
-//         nuovo_nodo->padre = y->padre;
-//         if (nuovo_nodo->chiave < y->chiave)
-//             y->dx = nuovo_nodo;
-//         else
-//             y->sx = nuovo_nodo;
-
-//         nuovo_nodo->sx = &NILL;
-//         nuovo_nodo->dx = &NILL;
-//         nuovo_nodo->colore = 'r';
-
-//         inserisci_fixup(albero, nuovo_nodo);
-//     }
-//     return;
-// }
 
 void ruota_sx(Nodo **albero, Nodo *nodo_perno)
 {
@@ -159,53 +184,10 @@ void ruota_dx(Nodo **albero, Nodo *nodo_perno)
         nodo_perno->padre->sx = y;
     else
         nodo_perno->padre->dx = y;
-        
+
     y->dx = nodo_perno;
     nodo_perno->padre = y;
 }
-
-// void ruota_sx(Nodo **albero, Nodo *nodo_perno)
-// {
-//     Nodo *y = nodo_perno->dx;
-//     nodo_perno->dx = y->sx;
-
-//     if (y->sx != &NILL)
-//         y->sx->padre = nodo_perno;
-
-//     y->padre = nodo_perno->padre;
-//     if (nodo_perno->padre == &NILL)
-//         *(albero) = y;
-//     else if (nodo_perno == nodo_perno->padre->sx)
-//         nodo_perno->padre->sx = y;
-//     else
-//         nodo_perno->padre->dx = y;
-
-//     y->sx = nodo_perno;
-//     nodo_perno->padre = y;
-
-//     return;
-// }
-
-// void ruota_dx(Nodo **albero, Nodo *nodo_perno)
-// {
-//     Nodo *y = nodo_perno->sx;
-//     nodo_perno->sx = y->dx;
-
-//     if (y->dx != &NILL)
-//         y->dx->padre = nodo_perno;
-
-//     y->padre = nodo_perno->padre;
-//     if (nodo_perno->padre == &NILL)
-//         *(albero) = y;
-//     else if (nodo_perno == nodo_perno->padre->dx)
-//         nodo_perno->padre->dx = y;
-//     else
-//         nodo_perno->padre->sx = y;
-        
-//     y->dx = nodo_perno;
-//     nodo_perno->padre = y;
-// }
-
 
 void inserisci_fixup(Nodo **albero, Nodo *nuovo_nodo)
 {
@@ -273,6 +255,170 @@ void stampa_in_ordine(Nodo *albero)
         stampa_in_ordine(albero->sx);
         printf("%4c-%d", albero->colore, albero->chiave);
         stampa_in_ordine(albero->dx);
+    }
+    return;
+}
+
+void trapianta(Nodo **albero, Nodo *u, Nodo *v)
+{
+    if (u->padre == &NILL)
+        (*albero) = v;
+    else if (u == u->padre->sx)
+        u->padre->sx = v;
+    else
+        u->padre->dx = v;
+
+    if (v != &NILL)
+        v->padre = u->padre;
+}
+
+Nodo *minimo(Nodo *nodo)
+{
+    while (nodo->sx != &NILL)
+        nodo = nodo->sx;
+
+    return nodo;
+}
+
+Nodo *massimo(Nodo *nodo)
+{
+    while (nodo->dx != &NILL)
+        nodo = nodo->dx;
+
+    return nodo;
+}
+
+Nodo *cerca(Nodo *radice, int chiave)
+{
+    if (radice == &NILL || chiave == radice->chiave)
+        return radice;
+    if (chiave < radice->chiave)
+        return cerca(radice->sx, chiave);
+    else
+        return cerca(radice->dx, chiave);
+}
+
+void elimina_nodo(Nodo **albero, Nodo *nodo)
+{
+    Nodo *y = nodo;
+    Nodo *x;
+    char colore_originale_y;
+
+    //verifico che il nodo esiste
+    if (cerca(nodo,nodo->chiave)== &NILL)
+    {
+        printf("Nodo inesistente!\n");
+        return;
+    }
+
+    //se si procedo con eliminazione
+    colore_originale_y = y->colore;
+    if (nodo->sx == &NILL)
+    {
+        x = nodo->dx;
+        trapianta(albero, nodo, nodo->dx);
+    }
+    else if (nodo->dx == &NILL)
+    {
+        x = nodo->sx;
+        trapianta(albero, nodo, nodo->sx);
+    }
+    else
+    {
+        y = minimo(nodo->dx);
+        colore_originale_y = y->colore;
+        x = y->dx;
+        if (y != nodo->dx)
+        {
+            trapianta(albero, y, y->dx);
+            y->dx = nodo->dx;
+            y->dx->padre = y;
+        }
+        else
+        {
+            x->padre = y;
+            trapianta(albero, nodo, y);
+            y->sx = nodo->sx;
+            y->sx->padre = y;
+            y->colore = nodo->colore;
+        }
+        if (colore_originale_y == 'n')
+            elimina_fixUp(albero, x);
+    }
+    return;
+}
+
+void elimina_fixUp(Nodo **albero, Nodo *nodo)
+{
+    Nodo *w;
+
+    while (nodo != &NILL && nodo->colore == 'n')
+    {
+        if (nodo == nodo->padre->sx)
+        {
+            w = nodo->padre->dx;
+            if (w->colore == 'r')
+            { // caso 1
+                w->colore = 'n';
+                nodo->padre->colore = 'r';
+                ruota_sx(albero, nodo->padre);
+                w = nodo->padre->dx;
+            }
+            if (w->sx->colore == 'n' && w->dx->colore == 'n')
+            { // caso 2
+                w->colore = 'r';
+                nodo = nodo->padre;
+            }
+            else
+            {
+                if (w->dx->colore == 'n')
+                { // caso 3
+                    w->sx->colore = 'n';
+                    w->colore = 'r';
+                    ruota_dx(albero, w);
+                    w = nodo->padre->dx;
+                }
+                // caso 4
+                w->colore = nodo->padre->colore;
+                nodo->padre->colore = 'n';
+                w->dx->colore = 'n';
+                ruota_sx(albero, nodo->padre);
+                nodo = (*albero);
+            }
+        }
+        else
+        { // come sopra ma con dx e sx scambiati
+            w = nodo->padre->sx;
+            if (w->colore == 'r')
+            { // caso 1
+                w->colore = 'n';
+                nodo->padre->colore = 'r';
+                ruota_dx(albero, nodo->padre);
+                w = nodo->padre->sx;
+            }
+            if (w->dx->colore == 'n' && w->sx->colore == 'n')
+            { // caso 2
+                w->colore = 'r';
+                nodo = nodo->padre;
+            }
+            else
+            {
+                if (w->sx->colore == 'n')
+                { // caso 3
+                    w->dx->colore = 'n';
+                    w->colore = 'r';
+                    ruota_sx(albero, w);
+                    w = nodo->padre->sx;
+                }
+                // caso 4
+                w->colore = nodo->padre->colore;
+                nodo->padre->colore = 'n';
+                w->sx->colore = 'n';
+                ruota_dx(albero, nodo->padre);
+                nodo = (*albero);
+            }
+        }
+        nodo->colore = 'n';
     }
     return;
 }
